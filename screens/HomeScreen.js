0792b629a2104177ac9aa3f95bfea2718b0ca820
ZarathosDeath <nando_rhapsody@hotmail.com>
2019-11-20 13:58:43 -0300

    added homepage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,104 +12,52 @@ import {
 } from 'react-native';
 
 import { MonoText } from '../components/StyledText';
+import Colors from '../constants/Colors';
+import { Ionicons } from '@expo/vector-icons'
 
+import MainButton from '../components/buttons/mainButton'
 
-export default class App extends Component {
-  state = {
-    todo: "This a message"
-  }
-  
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Skincare</Text>
-        <TextInput style={styles.input}></TextInput>    
-      </View>
-    );
-}
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ADD8E6',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  box: {
-    width:60,
-    height:60,
-    backgroundColor: 'blue'
-  },
-  input: {
-    paddingRight: 100,
-    height: 40,
-    borderColor: '#fff',
-    borderWidth: 1
-  }
-
-});
-
-
-/*
-export default function HomeScreen() {
+export default function HomeScreen(props) {
+  const {navigate} = props.navigation
   return (
     <View style={styles.container}>
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.contentContainer}>
+          {/* logo */}
         <View style={styles.welcomeContainer}>
           <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
+            source={require('../assets/images/skincare-logo.png')}
+            style={styles.logo}
           />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
+          <View style={styles.contentContainer}>
+            <Text style={styles.welcomeText}>Bem Vindo</Text>
           </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
+          {/* Main Menu */}
+          <View style={styles.menuContainer}>
+            <MainButton
+              text="Começar"
+              width="100%"
+              color={Colors.blue01}
+              icon={<Ionicons name="md-camera" size={20} style={styles.startIcon}/>}
+              backgroundColor={Colors.white} />
+            <MainButton
+              text="Sobre"
+              color={Colors.blue01}
+              width="100%"
+              icon={<Ionicons name="ios-information-circle" size={20} style={styles.aboutIcon}/>}
+              backgroundColor={Colors.white}
+              onPress={() => navigate('About')} />
+            <MainButton
+              text="Documentação"
+              color={Colors.blue01}
+              width="100%"
+              icon={<Ionicons name="logo-github" size={20} style={styles.aboutIcon}/>}
+              backgroundColor={Colors.white}
+              onPress={handleDocumentationPress} />
+          </View>
         </View>
       </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
     </View>
   );
 }
@@ -118,127 +66,65 @@ HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
 
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
+function handleAboutPress() {
+  navigate('About')
 }
 
-function handleLearnMorePress() {
+function handleDocumentationPress() {
   WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
+    'https://github.com/deeplearningunb/Skincare/'
   );
 }
 
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+    display: 'flex',
+    fontFamily: 'roboto',
+    backgroundColor: Colors.blue00,
   },
   contentContainer: {
     paddingTop: 30,
   },
   welcomeContainer: {
     alignItems: 'center',
-    marginTop: 10,
     marginBottom: 20,
+    flex: 1,
+    display: 'flex',
+    marginTop: 30,
+    padding: 20
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
+  logo: {
+    width: 300,
+    height: 150,
     resizeMode: 'contain',
     marginTop: 3,
     marginLeft: -10,
   },
-  getStartedContainer: {
+  menuContainer: {
+    flex: 1,
+    display: 'flex',
     alignItems: 'center',
-    marginHorizontal: 50,
+    marginHorizontal: 40,
   },
-  homeScreenFilename: {
-    marginVertical: 7,
+  welcomeText: {
+    fontSize: 30,
+    color: Colors.blue01,
+    fontWeight: "300",
+    marginBottom: 40
   },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
+  startIcon: {
+    color: Colors.blue01,
+    position: "relative",
+    left: 30,
+    zIndex: 8
   },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  aboutIcon: {
+    color: Colors.blue01,
+    position: "relative",
+    left: 20,
+    zIndex: 8
   },
 });
-*/
